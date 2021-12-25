44855ca41e479d09b4190e17aad3029bc458955d
sskurochkin <semenkurochkin@gmail.com>
2021-12-25 13:27:07 +0300

    add charForm

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/errorBoundary";
 import decoration from "../../resources/img/vision.png";
+import CharForm from "../charForm/CharForm";
 
 const MainPage = () => {
 	const [selectedChar, setChar] = useState(null);
@@ -20,13 +21,18 @@ const MainPage = () => {
 				<ErrorBoundary>
 					<CharList onCharSelected={onCharSelected} />
 				</ErrorBoundary>
-				<ErrorBoundary>
-					<CharInfo charId={selectedChar} />
-				</ErrorBoundary>
+				<div>
+					<ErrorBoundary>
+						<CharInfo charId={selectedChar} />
+					</ErrorBoundary>
+					<ErrorBoundary>
+						<CharForm />
+					</ErrorBoundary>
+				</div>
 			</div>
 			<img className='bg-decoration' src={decoration} alt='vision' />
 		</>
 	);
 };
 
-export default MainPage
+export default MainPage;
